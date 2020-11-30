641dadd5286d94a8cce75a0fe2b334b3f3f0ef1c
maryam <maryam@rivile.local>
2020-11-30 10:24:34 +0200

    Done

---
+++ dist/js/main.js
@@ -0,0 +1,44 @@
+$('#nav-menu').css('max-height', '0px')
+$('.menu-icon').click(function () {
+    if ($('#nav-menu').css('max-height') == '0px') {
+        $('#nav-menu').css('max-height', '200px');
+    }else{
+        $('#nav-menu').css('max-height', '0px')
+    }
+})
+
+$('#subscribe-btn').click(function (e) {
+    e.preventDefault();
+    $('#myModal').fadeIn();
+})
+
+$('.close').click(function () {
+    $('#myModal').fadeOut();
+})
+
+$(window).scroll(function () {
+    sc = $(this).scrollTop();
+    if (sc > 800) {
+        $('.scrolltop').fadeIn();
+    } else {
+        $(".scrolltop").fadeOut();
+    }
+})
+
+$('.scrolltop').click(function () {
+    $('body , html').animate({
+        scrollTop: 0
+    }, 500)
+})
+
+$('#nav-menu a').click(function () {
+    $('body , html').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 20
+    })
+})
+
+$('.small-img-row img').each(function () {
+    $(this).click(function () {
+        $('#service-img').attr('src', $(this).attr('src'))
+    })
+})
